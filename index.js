d56fcffd3b34bd51a9545abb7246d882a5489e7c
Ande1901 <andefranata@gmail.com>
2024-09-22 23:56:59 +0700

    multer

--- index.js
+++ index.js
@@ -1,10 +1,128 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 5000;
+const path = require("path");
+const { title } = require("process");
+const config = require("./config/config.json");
+const { Sequelize, QueryTypes, where } = require("sequelize");
+const { type } = require("os");
+const { UPDATE } = require("sequelize/lib/query-types");
+const upload = require("./middlewares/upload-file");
 
-app.get("/", (req, res) => {
-  res.send("Hallo Dunia!");
-});
+const sequelize = new Sequelize(config.development);
+
+const blogModel = require("./models").blog;
+
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "assets/views"));
+app.use("/uploads", express.static(path.join(__dirname, "./uploads")));
+app.use("/assets", express.static(path.join(__dirname, "assets")));
+app.use(express.urlencoded({ extended: true }));
+
+// Routing
+app.get("/", home);
+app.get("/Blog", Blog);
+app.get("/add-blog", addBlogView);
+app.post("/add-blog", upload.single("image"), addBlog);
+app.get("/delete-blog/:id", deleteBlog);
+app.get("/edit-blog/:id", editBlogView);
+app.post("/edit-blog/:id", editBlog);
+app.get("/contact", contactMe);
+app.get("/Blog-detail/:id", blogDetail);
+app.get("/testimonal", testimonials);
+
+const dataBlog = [];
+
+function home(req, res) {
+  res.render("index");
+}
+
+async function Blog(req, res) {
+  const result = await blogModel.findAll();
+
+  res.render("Blog", { dataBlog: result });
+}
+
+async function editBlogView(req, res) {
+  const { id } = req.params;
+
+  const result = await blogModel.findOne({
+    where: {
+      id: id,
+    },
+  });
+
+  if (!result) return res.render("not-found");
+  res.render("edit-blog", { dataBlog: result });
+}
+
+async function editBlog(req, res) {
+  try {
+    const { id } = req.params;
+    const { title, content } = req.body;
+
+    const query = `UPDATE public.blogs SET title='${title}', content='${content}' WHERE id=${id}`;
+    await sequelize.query(query, { type: QueryTypes.UPDATE });
+
+    res.redirect("/Blog");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function addBlogView(req, res) {
+  res.render("add-blog");
+}
+
+async function addBlog(req, res) {
+  const { title, content } = req.body;
+  const imagePath = req.file.path;
+  await blogModel.create({
+    title: title,
+    content: content,
+    image: imagePath,
+  });
+  res.redirect("/Blog");
+}
+
+async function deleteBlog(req, res) {
+  let { id } = req.params;
+
+  let result = await blogModel.findOne({
+    where: {
+      id: id,
+    },
+  });
+
+  if (!result) return res.render("not-found");
+
+  await blogModel.destroy({
+    where: {
+      id: id,
+    },
+  });
+  res.redirect("/Blog");
+}
+
+function contactMe(req, res) {
+  res.render("contact");
+}
+
+async function blogDetail(req, res) {
+  const { id } = req.params;
+  const result = await blogModel.findOne({
+    where: {
+      id: id,
+    },
+  });
+
+  if (!result) return res.render("not-found");
+  res.render("Blog-detail", { dataBlog: result });
+}
+
+function testimonials(req, res) {
+  res.render("testimonal");
+}
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
---
+++ models/blog.js
@@ -0,0 +1,27 @@
+"use strict";
+const { Model, ARRAY, Sequelize } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class blog extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  blog.init(
+    {
+      title: DataTypes.STRING,
+      content: DataTypes.STRING,
+      image: DataTypes.STRING,
+      technologies: Sequelize.ARRAY(DataTypes.STRING),
+    },
+    {
+      sequelize,
+      modelName: "blog",
+    }
+  );
+  return blog;
+};
